39a1d71756adc993bdc9209cb663ec6d1abfb0f4
moh2099 <moh_2323@hotmail.com>
2020-05-19 09:10:46 +0300

    asd

--- would-you-rather/src/actions/authedUserActions.js
+++ would-you-rather/src/actions/authedUserActions.js
@@ -1,6 +1,6 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 
-export function authedUserActionCreator (authedUserId) {
+export function setAuthedUser (authedUserId) {
     return {
       type: SET_AUTHED_USER,
       authedUserId,
--- would-you-rather/src/actions/initState.js
+++ would-you-rather/src/actions/initState.js
@@ -2,22 +2,18 @@ import { getInitState } from '../utils/api'
 import { userActionCreator } from '../actions/usersActions'
 //import { questionActionCreator } from '../actions/questionsActions'
 import { getQuestions } from '../actions/questionsActions'
-import { authedUserActionCreator } from '../actions/autherUserActions'
+import { setAuthedUser } from '../actions/authedUserActions'
 //import { showLoading, hideLoading } from 'react-redux-loading'
-import { setAuthedUser } from '../actions/authedUser'
-
+ 
 const authedUserId = 'tylermcginnis'
 
 export const initState = () => {
   return (dispatch) => {
-    return getInitState().then(({ users, questions }) => { 
-    //  let res_json = JSON.parse(JSON.stringify(res)) //convert Object to json to access object keys as properties
-      dispatch(userActionCreator(users))
-      dispatch(getQuestions(questions))
-      dispatch(setAuthedUser(authedUserId))
+    return getInitState().then((res) => { 
+       dispatch(userActionCreator(res.users))
+       dispatch(getQuestions(res.questions))
+       dispatch(setAuthedUser(authedUserId))
 
-  //      dispatch(authedUserActionCreator(authedUserId))
-       // console.log(res)
     })
       // .then(({ users, tweets }) => {
       //   // dispatch(receiveUsers(users))
